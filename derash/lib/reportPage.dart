8a795e70616656d161587a875f6a8c9e16d65271
Dagim Fikru <dagimfikru@gmail.com>
2022-05-07 16:25:36 +0000

    start working on report page

---
+++ derash/lib/reportPage.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: file_names, prefer_const_constructors, camel_case_types
+import 'package:flutter/material.dart';
+class repotPage extends StatelessWidget {
+  const repotPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Report page'),
+        backgroundColor: Colors.brown,
+        centerTitle: true,
+      ),
+    );
+  }
+}
