a87496b09ef0630e259c4e06073181e14921fd42
kidussintayehu <kidussintayehu60@gmail.com>
2022-06-01 19:52:55 +0300

    delete unnecessary filesgi

--- derash/lib/models/drash_pages.dart
+++
@@ -1,8 +0,0 @@
-class DerashPages {
-  static String stationsPath = '/stations';
-  static String loginPath = '/login';
-  static String profilePath = '/profile';
-  static String editstationPath = '/editstation';
-  static String adminPath = '/adminscreen';
-  static String usersPath = '/usersscreen';
-}
