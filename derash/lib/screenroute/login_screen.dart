61e223d8c5e6279d056dc779593d1dbaccefe03d
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2022-06-02 22:41:02 +0300

    init

---
+++ derash/lib/screenroute/login_screen.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( title: Text("splash"),),
+      
+      body: Center(
+       child: SizedBox(
+         height: 15,
+       )
+
+      ) 
+      
+       );
+  }
+}
